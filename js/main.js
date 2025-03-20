8b7509a2623e98854de828aaf4770f0ab0d2fb2b
robot756 <shcherbakovivan_2003@mail.ru>
2025-03-21 01:54:45 +0300

    задание 5.1

--- js/main.js
+++ js/main.js
@@ -1,63 +1,42 @@
-const massive = [];
+import {
+  massiveDescriptionPhotos, NAMES, TEXT_MESSAGES, TEXT_DESCRIPTION,
+  randomValueComment, minLikes, maxLikes, minAvatars, maxAvatars
+}
+  from './data.js';
 
-const names = [
-  'Алексей', 'Борис', 'Владимир', 'Григорий', 'Дмитрий',
-  'Евгений', 'Жанна', 'Зоя', 'Иван', 'Кирилл',
-  'Лариса', 'Марина', 'Наталья', 'Олег', 'Павел',
-  'Роман', 'Светлана', 'Татьяна', 'Ульяна', 'Фёдор',
-  'Харитон', 'Цезарь', 'Чеслав', 'Эльвира', 'Юрий'
-];
+let getPhotos = () => {
 
-const textMessage = [
-  'Всё отлично!',
-  'В целом всё неплохо.Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ? !'
-];
-
-const val = function () {
-  let counter = 0;
-  const randomIdComment = [];
-  for (let k = 23; k <= 150; k++) {
-    randomIdComment.push(k);
-  }
-
-  return function closure () {
-    const start = randomIdComment[counter];
-    counter++;
-    return start;
-  };
-};
-
-const randomVal = val();
-
-function abs() {
   for (let i = 0; i < 25; i++) {
-    const randomAvatar = Math.round(Math.random() * (6 - 1) + 1);
-    const randomValue = Math.round(Math.random() * (200 - 15) + 15);
-    const obj = {};
+    let randomValueLikes = Math.floor(Math.random() * (maxLikes - minLikes) + minLikes);
+    let randomDescription = TEXT_DESCRIPTION[Math.floor(Math.random() * TEXT_DESCRIPTION.length)];
 
-    obj['id'] = i + 1;
-    obj['url'] = `photos/${i + 1}.jpg`;
-    obj['description'] = 'New photo today!';
-    obj['likes'] = randomValue;
-
-    const objInfo = {
-      id: randomVal(),
-      avatar: `img/avatar-${randomAvatar}.svg`
+    let photo = {
+      'id': i + 1,
+      'url': `photos/${i + 1}.jpg`,
+      'description': randomDescription,
+      'likes': randomValueLikes,
+      'comments': [],
     };
 
-    const randomName = names[Math.floor(Math.random() * 25)];
-    const randomMessage = textMessage[Math.floor(Math.random() * textMessage.length)];
+    for (let j = 0; j < Math.floor(Math.random() * 31); j++) {
+      let randomName = NAMES[Math.floor(Math.random() * NAMES.length)];
+      let randomMessage = TEXT_MESSAGES[Math.floor(Math.random() * TEXT_MESSAGES.length)];
+      let randomAvatar = Math.floor(Math.random() * maxAvatars) + minAvatars;
 
-    objInfo['name'] = randomName;
-    objInfo['message'] = randomMessage;
-    obj['message'] = objInfo;
+      let comment = {
+        'id': randomValueComment(),
+        'avatar': `img/avatar-${randomAvatar}.svg`,
+        'message': randomMessage,
+        'name': randomName,
+      };
 
-    massive.push(obj);
+      photo['comments'].push(comment);
+    };
+
+    massiveDescriptionPhotos.push(photo);
   }
 
-  return massive;
+  return massiveDescriptionPhotos;
 }
+
+console.log(JSON.stringify(getPhotos(), null, 2));
